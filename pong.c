b04ca37112f3bcee97db01c09bf2ca800d4ea744
Tatyana Kharitonova <mylyahight@gmail.com>
2023-06-25 12:35:58 +0300

    basic screen with elements

--- pong.c
+++ pong.c
@@ -1,36 +1,87 @@
 #include <stdio.h>
 
-int ball_x = 40;
-int ball_y = 13;
+// константы
+#define x_min 1
+#define x_max 80
+#define y_min 1
+#define y_max 25
+#define coord_net_1 39
+#define coord_net_2 40
+
+// глобальные переменные
+int racket_p1_x = 1;
+int racket_p1_y_1 = 11;
+int racket_p1_y_2 = 12;
+int racket_p1_y_3 = 13;
+int racket_p2_x = 80;
+int racket_p2_y_1 = 11;
+int racket_p2_y_2 = 12;
+int racket_p2_y_3 = 13;
+int ball_x = 24;
+int ball_y = 12;
+
+void render_play_field(void);
+int get_is_racket_p1_coords(int x, int y);
+int get_is_racket_p2_coords(int x, int y);
 
 int main()
 {
-    int x_max = 80;
-    int y_max = 25;
-    for (int i = 0; i < 82; i++)
-    {
-        printf("_");
-    }
-    printf("\n");
+    render_play_field();
+}
 
-    for (int y = y_max; y >= 1; y--)
+void render_play_field()
+{
+    for (int i = y_min; i <= y_max; i++)
     {
-        printf("|");
-        for (int x = 1; x <= x_max; x++)
+        for (int j = x_min; j <= x_max; j++)
         {
-            if (ball_x == x && ball_y == y)
+            if (get_is_racket_p1_coords(j, i) == 1 || get_is_racket_p2_coords(j, i) == 1)
+            {
+                printf("|");
+
+                continue;
+            }
+            if (j == ball_x && i == ball_y)
             {
                 printf("*");
+
+                continue;
+            }
+            if (j == coord_net_1 || j == coord_net_2)
+            {
+
+                printf("|");
             }
             else
             {
                 printf(" ");
             }
         }
-        printf("|\n");
+        printf("\n");
     }
-    for (int i = 0; i < 82; i++)
+    printf("\n");
+}
+
+int get_is_racket_p1_coords(int x, int y)
+{
+    if (x == racket_p1_x && (y == racket_p1_y_1 || y == racket_p1_y_2 || y == racket_p1_y_3))
     {
-        printf("_");
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int get_is_racket_p2_coords(int x, int y)
+{
+    if (x == racket_p2_x && (y == racket_p2_y_1 || y == racket_p2_y_2 || y == racket_p2_y_3))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
 }
