0b711e6d5a664343ff37fb22453e5710ca525fdd
Tatyana Kharitonova <mylyahight@gmail.com>
2023-06-25 14:22:55 +0300

    remove global variables and defined expressions

--- pong.c
+++ pong.c
@@ -1,47 +1,43 @@
 #include <stdio.h>
 
-// константы
-#define x_min 1
-#define x_max 80
-#define y_min 1
-#define y_max 25
-#define coord_net_1 39
-#define coord_net_2 40
-
-// глобальные переменные
-int racket_p1_x = 1;
-int racket_p1_y_1 = 11;
-int racket_p1_y_2 = 12;
-int racket_p1_y_3 = 13;
-int racket_p2_x = 80;
-int racket_p2_y_1 = 11;
-int racket_p2_y_2 = 12;
-int racket_p2_y_3 = 13;
-int ball_x = 24;
-int ball_y = 12;
-
-void render_play_field(void);
-int get_is_racket_p1_coords(int x, int y);
-int get_is_racket_p2_coords(int x, int y);
+void render_play_field(int x_min, int x_max, int y_min, int y_max, int r1x1, int r1y1, int r1y2, int r1y3, int r2x1, int r2y1, int r2y2, int r2y3, int bx, int by, int coord_net_1, int coord_net_2);
+int get_is_racket_coords(int x, int y, int x1, int y1, int y2, int y3);
 
 int main()
 {
-    render_play_field();
+    int x_min = 1;
+    int x_max = 80;
+    int y_min = 1;
+    int y_max = 25;
+    int coord_net_1 = 39;
+    int coord_net_2 = 40;
+    int racket_p1_x = 1;
+    int racket_p1_y_1 = 11;
+    int racket_p1_y_2 = 12;
+    int racket_p1_y_3 = 13;
+    int racket_p2_x = 80;
+    int racket_p2_y_1 = 11;
+    int racket_p2_y_2 = 12;
+    int racket_p2_y_3 = 13;
+    int ball_x = 24;
+    int ball_y = 12;
+
+    render_play_field(x_min, x_max, y_min, y_max, racket_p1_x, racket_p1_y_1, racket_p1_y_2, racket_p1_y_3, racket_p2_x, racket_p2_y_1, racket_p2_y_2, racket_p2_y_3, ball_x, ball_y, coord_net_1, coord_net_2);
 }
 
-void render_play_field()
+void render_play_field(int x_min, int x_max, int y_min, int y_max, int r1x1, int r1y1, int r1y2, int r1y3, int r2x1, int r2y1, int r2y2, int r2y3, int bx, int by, int coord_net_1, int coord_net_2)
 {
     for (int i = y_min; i <= y_max; i++)
     {
         for (int j = x_min; j <= x_max; j++)
         {
-            if (get_is_racket_p1_coords(j, i) == 1 || get_is_racket_p2_coords(j, i) == 1)
+            if (get_is_racket_coords(j, i, r1x1, r1y1, r1y2, r1y3) == 1 || get_is_racket_coords(j, i, r2x1, r2y1, r2y2, r2y3) == 1)
             {
                 printf("|");
 
                 continue;
             }
-            if (j == ball_x && i == ball_y)
+            if (j == bx && i == by)
             {
                 printf("*");
 
@@ -62,21 +58,9 @@ void render_play_field()
     printf("\n");
 }
 
-int get_is_racket_p1_coords(int x, int y)
+int get_is_racket_coords(int x, int y, int x1, int y1, int y2, int y3)
 {
-    if (x == racket_p1_x && (y == racket_p1_y_1 || y == racket_p1_y_2 || y == racket_p1_y_3))
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
-int get_is_racket_p2_coords(int x, int y)
-{
-    if (x == racket_p2_x && (y == racket_p2_y_1 || y == racket_p2_y_2 || y == racket_p2_y_3))
+    if (x == x1 && (y == y1 || y == y2 || y == y3))
     {
         return 1;
     }
